208c217d108011d3f3aed08d0d98598fb4d8f9d8
emmxy-ops <folorunshoemma360@gmail.com>
2026-06-30 10:25:20 +0100

    Fix string quotes and update test assertion

--- test.py
+++ test.py
@@ -4,12 +4,12 @@ from app import app
 
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True
+    app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
 
 
 def test_app_is_working(client):
-    response = client.get('/')
+    response = client.get("/")
     assert response.status_code == 200
-    assert b"Hello World!" in response.data
+    assert b"Hello CICD World!" in response.data
